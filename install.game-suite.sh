cc1fc57c77881603ff68122f0726cf272eb75ac1
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-06 04:13:22 +0200

    Update install.game-suite.sh

--- install.game-suite.sh
+++ install.game-suite.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
 
-# Change Max_Map_Count
-sudo sysctl -w vm.max_map_count=655300
+# Get the current value of map_max_count
+current_value=$(sysctl -n vm.max_map_count)
+
+# Check if the current value is less than 655300
+if [ "$current_value" -lt 655300 ]; then
+    echo "Adjusting vm.max_map_count to 655300"
+    sudo sysctl -w vm.max_map_count=655300
+else
+    echo "vm.max_map_count is already sufficient: $current_value"
+fi
+
+
