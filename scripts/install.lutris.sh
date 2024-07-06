2599f511982e0fb97d36fe7ba6af25554dc870a3
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-06 05:42:10 +0200

    Update install.lutris.sh

--- scripts/install.lutris.sh
+++ scripts/install.lutris.sh
@@ -3,17 +3,17 @@ if command -v apt-get &> /dev/null; then
     # Debian/Ubuntu based systems
     echo "Detected APT package manager."
     sudo apt-get update
-    sudo apt-get install -y steam heroic lutris wine-stable protontricks winetricks emudeck pegasus-frontend
+    sudo apt-get install -y lutris
 
 elif command -v dnf &> /dev/null; then
     # Fedora
     echo "Detected DNF package manager."
-    sudo dnf install -y steam heroic lutris wine protontricks winetricks emudeck pegasus-frontend
+    sudo dnf install -y lutris
 
 elif command -v pacman &> /dev/null; then
     # Arch Linux
     echo "Detected Pacman package manager."
-    sudo pacman -Syu --noconfirm steam heroic lutris wine-stable protontricks winetricks emudeck pegasus-frontend
+    sudo pacman -Syu --noconfirm lutris
 
 else
     echo "Unsupported distribution. Exiting."
