8c064b51979a0b1bc7ab6a0fc83186082e1dad6a
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-06 05:19:48 +0200

    Update install.game-suite.sh

--- install.game-suite.sh
+++ install.game-suite.sh
@@ -11,7 +11,36 @@ else
     echo "vm.max_map_count is already sufficient: $current_value"
 fi
 
-#############################################################################
+########### FLATPAK PREREQUSITE
+# Set up Flatpak
+echo "Setting up Flatpak..."
+
+# Check which package manager is available
+if command -v apt-get &> /dev/null; then
+    # Debian/Ubuntu based systems
+    echo "Detected APT package manager."
+    sudo apt-get update
+    sudo apt-get install -y flatpak
+
+elif command -v dnf &> /dev/null; then
+    # Fedora
+    echo "Detected DNF package manager."
+    sudo dnf install -y flatpak
+
+elif command -v pacman &> /dev/null; then
+    # Arch Linux
+    echo "Detected Pacman package manager."
+    sudo pacman -Syu --noconfirm flatpak
+
+else
+    echo "Unsupported distribution. Exiting."
+    exit 1
+fi
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Flatpak installed and set up successfully."
+
 #############################################################################
 
 ########### WINE & WineTricks
